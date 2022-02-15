865690f7a4d59f964bf517f3a0a17139393a52d7
elash1312 <lasherin1312@gmail.com>
2022-02-15 00:21:38 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainName = [];
+  let pronoun = ["the", "A"];
+  let adj = ["gigantic", "hysterical", "happy", "professional"];
+  let noun = ["homechef", "crybaby", "blogger", "programmer"];
+  let domain = [".com", ".net", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          domainName.push("  " + pronoun[i] + adj[j] + noun[k] + domain[l]);
+        }
+      }
+    }
+  }
+  console.log(domainName);
+  document.getElementById("domain.results").innerHTML = domainName;
 };
