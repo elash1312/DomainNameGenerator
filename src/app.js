9138b9798bdcf29189b35df5ae7443a015609f2b
elash1312 <lasherin1312@gmail.com>
2022-04-02 16:27:47 +0000

    li tag fixed and styling added

--- src/app.js
+++ src/app.js
@@ -5,19 +5,23 @@ import "./style.css";
 window.onload = () => {
   let domainName = [];
   let pronoun = ["the", "a", "one"];
-  let adj = ["hysterical", "happy", "professional"];
-  let noun = ["homechef", "skater", "blogger", "programmer"];
+  let adj = ["hyphy", "happy", "professional"];
+  let noun = ["homechef", "skater", "homesteader", "programmer"];
   let domain = [".com", ".net"];
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
         for (let l = 0; l < domain.length; l++) {
-          domainName.push("  " + pronoun[i] + adj[j] + noun[k] + domain[l]);
+          domainName.push(" " + pronoun[i] + adj[j] + noun[k] + domain[l]);
         }
       }
     }
   }
   console.log(domainName);
-  document.getElementById("domain.results").innerHTML = domainName;
+  document.getElementById("domain.results").innerHTML = domainName
+    .map((item, index) => {
+      return "<div class='domain>" + item + "</div>";
+    })
+    .join("");
 };
